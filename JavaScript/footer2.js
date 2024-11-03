d8b6ac7715d035d35021c035f2c80bdb4f530e3c
JacquelineDeniseHetz <jacqueline-denise.hetz1@iu-study.org>
2024-11-03 16:49:09 +0100

    0.4.9.6.11

---
+++ JavaScript/footer2.js
@@ -0,0 +1,7 @@
+// Footer einbinden
+fetch('../html/footer2.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('footer-placeholder').innerHTML = data;
+})
+.catch(error => console.log('Footer konnte nicht geladen werden:', error));
