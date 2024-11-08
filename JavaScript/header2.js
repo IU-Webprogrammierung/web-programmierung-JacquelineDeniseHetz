647ccf8568a587e13d7fe4ec894f54c990a479aa
JacquelineDeniseHetz <jacqueline-denise.hetz1@iu-study.org>
2024-11-08 14:30:37 +0100

    0.4.9.6.15 - Header auf Portfolio.html ausgelagert, damit dieses einfacher in die anderen Seiten eingefügt werden kann

--- JavaScript/header2.js
+++ JavaScript/header2.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('../html/header.html')
+    fetch('../html/header2.html')
         .then(response => response.text())
         .then(data => {
-            document.getElementById('header-placeholder').innerHTML = data;
+            document.getElementById('header2-placeholder').innerHTML = data;
         })
         .catch(err => console.error('Fehler beim Laden des Headers:', err));
 });
